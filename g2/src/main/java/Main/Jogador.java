92f5a20c43c36bc00e29a48bb263c34fa060573d
Gabriel Pilger <gabrielsupermon@gmail.com>
2021-06-07 15:43:24 -0300

    Criacao de um metodo de analise da entrada da jogada do jogador para melhor assimilacao dentro do codigo main e atualizacao do texto na classe jogador

--- g2/src/main/java/Main/Jogador.java
+++ g2/src/main/java/Main/Jogador.java
@@ -1,8 +1,27 @@
-/* Classe jogador para controle e padronização de variaveis de moedas*/
+/* Classe jogador para controle e padronização de variaveis de moedas e metodo de analise
+de escolha de jogada para implementação no codigo principal*/
 package Main;
 
 
 public class Jogador {
     String nome = "Jogador";
     int moedas = 1;
+    
+    
+    public boolean decisao(String escolha){
+        if("1".equals(escolha)){
+            return true;
+        }
+        else if("2".equals(escolha)){
+            return false;
+        }
+        return false;
+    }
+    
+    
+    
+    
+    
+    
+    
 }
